6f7c1c8f836fc94aea11b108043f16aeae229296
mfrancis95 <mikefrancis95@gmail.com>
2016-01-28 20:34:21 -0500

    Add --file argument for reading from files instead of stdin

--- quick-mail.py
+++ quick-mail.py
@@ -4,11 +4,17 @@ import sys
 from smtplib import SMTP_SSL
 
 parser = ArgumentParser("quick-mail")
+parser.add_argument("-f", "--file")
 parser.add_argument("to")
 parser.add_argument("message")
 args = parser.parse_args()
 
-config = load(sys.stdin)
+file = args.file
+if file:
+    with open(file) as f:
+        config = load(f)
+else:
+    config = load(sys.stdin)
 
 with SMTP_SSL(config["host"], config.get("port", 465)) as smtp:
     username = config["username"]
