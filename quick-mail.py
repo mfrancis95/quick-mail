a9bfc8caaabd77f185947d73d9700824d3b9a6cb
mfrancis95 <mikefrancis95@gmail.com>
2016-02-18 10:43:34 -0500

    Improve email sending

--- quick-mail.py
+++ quick-mail.py
@@ -2,6 +2,8 @@ from argparse import ArgumentParser
 from json import load
 import sys
 from smtplib import SMTP_SSL
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 parser = ArgumentParser("quick-mail")
 parser.add_argument("-f", "--file")
@@ -19,4 +21,8 @@ else:
 with SMTP_SSL(config["host"], config.get("port", 465)) as smtp:
     username = config["username"]
     smtp.login(username, config["password"])
-    smtp.sendmail(username, args.to, args.message)
+    message = MIMEMultipart()
+    message["From"] = username
+    to = message["To"] = args.to
+    message.attach(MIMEText(args.message, "plain"))
+    smtp.send_message(message, username, to)
